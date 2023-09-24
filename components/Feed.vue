2ed325b73d553c578df2e5dbbd9173770924f222
xdeq <celldwellerq@gmail.com>
2023-09-24 16:27:10 +0300

    feat(feed): tooltip for stats

--- components/Feed.vue
+++ components/Feed.vue
@@ -2,6 +2,9 @@
 /** Services */
 import { comma, formatBytes } from "@/services/utils"
 
+/** UI */
+import Tooltip from "@/components/ui/Tooltip.vue"
+
 /** Store */
 import { useAppStore } from "@/store/app"
 const appStore = useAppStore()
@@ -23,35 +26,49 @@ const head = computed(() => appStore.head)
 
 				<div :class="$style.dot" />
 
-				<Flex align="center" gap="6" :class="$style.stat">
-					<Icon name="coins" size="12" color="secondary" :class="$style.icon" />
-					<Flex align="center" gap="4">
-						<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Supply:</Text>
-						<Text size="12" weight="600" noWrap :class="$style.value">1.07B TIA</Text>
+				<Tooltip>
+					<Flex align="center" gap="6" :class="$style.stat">
+						<Icon name="coins" size="12" color="secondary" :class="$style.icon" />
+						<Flex align="center" gap="4">
+							<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Supply:</Text>
+							<Text size="12" weight="600" noWrap :class="$style.value">1.07B TIA</Text>
+						</Flex>
 					</Flex>
-				</Flex>
+
+					<template #content>
+						{{ comma(1073647108) }}
+					</template>
+				</Tooltip>
 
 				<div :class="$style.dot" />
 
-				<Flex align="center" gap="6" :class="$style.stat">
-					<Icon name="folder" size="12" color="secondary" :class="$style.icon" />
-					<Flex align="center" gap="4">
-						<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Blobs Size:</Text>
-						<Text size="12" weight="600" noWrap :class="$style.value">{{ formatBytes(head.total_blobs_size) }}</Text>
+				<Tooltip>
+					<Flex align="center" gap="6" :class="$style.stat">
+						<Icon name="folder" size="12" color="secondary" :class="$style.icon" />
+						<Flex align="center" gap="4">
+							<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Blobs Size:</Text>
+							<Text size="12" weight="600" noWrap :class="$style.value">{{ formatBytes(head.total_blobs_size) }}</Text>
+						</Flex>
 					</Flex>
-				</Flex>
+
+					<template #content> {{ comma(head.total_blobs_size) }} Bytes</template>
+				</Tooltip>
 
 				<div :class="$style.dot" />
 
-				<Flex align="center" gap="6" :class="$style.stat">
-					<Icon name="tag" size="12" color="secondary" :class="$style.icon" />
-					<Flex align="center" gap="4">
-						<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Fees:</Text>
-						<Text size="12" weight="600" noWrap :class="$style.value"
-							>{{ comma(parseInt(head.total_fee / 1_000_000)) }} TIA</Text
-						>
+				<Tooltip>
+					<Flex align="center" gap="6" :class="$style.stat">
+						<Icon name="tag" size="12" color="secondary" :class="$style.icon" />
+						<Flex align="center" gap="4">
+							<Text size="12" weight="500" color="tertiary" noWrap :class="$style.key">Total Fees:</Text>
+							<Text size="12" weight="600" noWrap :class="$style.value">
+								{{ comma(parseInt(head.total_fee / 1_000_000)) }} TIA
+							</Text>
+						</Flex>
 					</Flex>
-				</Flex>
+
+					<template #content> {{ comma(head.total_fee) }} UTIA </template>
+				</Tooltip>
 			</Flex>
 
 			<!-- <Flex align="center" gap="6" :class="$style.stat">
