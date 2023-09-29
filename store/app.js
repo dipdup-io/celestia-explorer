6526cd5c4aab3efad72160f2d2f3d7d6629d7986
xdeq <celldwellerq@gmail.com>
2023-09-29 16:07:50 +0300

    fix default state for head

--- store/app.js
+++ store/app.js
@@ -1,5 +1,14 @@
 export const useAppStore = defineStore("app", () => {
-	const head = ref()
+	const head = ref({
+		last_height: 0,
+		hash: "",
+		last_time: new Date(),
+		total_tx: 0,
+		total_accounts: 0,
+		total_fee: 0,
+		total_blobs_size: 0,
+		total_supply: 0,
+	})
 	const latestBlocks = ref([])
 
 	return { head, latestBlocks }
